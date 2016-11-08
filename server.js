f8ee42e17cb57cce880f33b4a39c6faa89f70aee
Tyler Geerdts <tygee713@gmail.com>
2016-11-08 15:27:03 -0500

    Finished back-end logic

--- server.js
+++ server.js
@@ -2,43 +2,26 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+var Burger = require('./models')["Burger"]
+Burger.sync();
+
 var app = express();
+
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : '',
-//   database : ''
-// });
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  };
-
-  console.log('connected as id ' + connection.threadId);
-});
-
-//Sample GET request
-app.get('/', function(req, res) {
-  
-});
-
-//Sample POST request
-app.post('/', function(req, res) {
-  
-});
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
 var port = 3000;
 app.listen(port, function() {
