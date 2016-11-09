ca555bdac7a99ec1dc1cb1a0d09a4d2a47896c19
Tyler Geerdts <tygee713@gmail.com>
2016-11-09 16:32:04 -0500

    changed port for Heroku

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 3000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log('App listening on PORT: ' + port);
 });
