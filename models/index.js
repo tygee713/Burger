ceae9c200107f597030cb47b97d5454fd63bfcc9
Tyler Geerdts <tygee713@gmail.com>
2016-11-09 22:03:43 -0500

    trying to fix heroku

--- models/index.js
+++ models/index.js
@@ -8,23 +8,23 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 // var Sequelize = require('sequelize'), 
-// var connection;
-// if (process.env.JAWSDB_URL) {
-//   connection = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   connection = new Sequelize('burgerDB', 'root', 'password', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: '3306'
-//   });
-// }
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new Sequelize('burgerDB', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3306'
+  });
+}
 
 fs
   .readdirSync(__dirname)
